6cfa7cb3b5bf37b1ad06d1d6b04c6e6f40473fc3
CyberShuo <117975967+Cyber-Shuo@users.noreply.github.com>
2023-07-18 20:56:52 +0800

    Update AL_hist_V1.0.py

--- AL_hist_V1.0.py
+++ AL_hist_V1.0.py
@@ -1,23 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from pathlib import Path
 from matplotlib import cm
 from scipy.optimize import curve_fit
-import pandas as pd
 
 AL_value, error_value, sample_name = [],[],[]
-lines = np.loadtxt('AL_DATA_PATH', dtype = str, comments='#', delimiter=None, converters=None, skiprows=0, usecols=None, unpack=False, ndmin=0)
+lines = np.loadtxt('D:\\2021-2024-data\python_fig\python_AL\python_data_hist\\AL_data.txt', dtype = str, comments='#', delimiter=None, converters=None, skiprows=0, usecols=None, unpack=False, ndmin=0)
 for line in lines:
     AL_value.append(float(line[0]))
     error_value.append(float(line[1]))
     sample_name.append(line[2])
 print(AL_value, error_value, sample_name)
 
-fig, ax1 = plt.subplots()
+
+plt.rcParams['font.sans-serif'] = ['simHei']
+fig, ax1 = plt.subplots(figsize = (16, 4))
 ax1.set_xlabel('Attenuation length/m')
 ax1.set_ylabel('Sample Name')
 
-norm = plt.Normalize(20,35)
+# get a list of colours based on attenuation length
+norm = plt.Normalize(17,40)
 map_vir = cm.get_cmap(name = 'Blues')
 colors = map_vir(norm(AL_value))
 
@@ -25,9 +26,10 @@ ax1.barh(sample_name, AL_value, color=colors, xerr = error_value, error_kw = {'e
 
 sm = cm.ScalarMappable(cmap = map_vir, norm = norm)
 
+# numerical display on the column
 for a,b,c in zip(AL_value, sample_name, error_value):  
     ax1.text(10, b, a, ha='center', va='center', fontsize=11, weight = 'semibold', color = 'black')
 
-plt.legend(loc = "upper right")
+# plt.legend(loc = "upper right")
 # plt.colorbar(sm)
 plt.show()
